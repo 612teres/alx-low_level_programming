a93e6c39d30089b88c16cd2319f7ceb9a2358688
612teres <612teres@gmail.com>
2023-12-15 04:22:20 -0800

    Manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,46 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /*
- *
  * main - checks the code
- * b: function parameter(bianry string)
+ * b: binary string
  * length: length of the binary string
  * result: the converted binary number
  * Return:result and 0 when b is NULL
  */
-unsigned int result = 0;
-int length;
-int a;
-
 unsigned int binary_to_uint(const char *b)
 {
-if (b == NULL || strlen(b) == 0){
-printf("Empty string\n");
-return 0;
-}
-
-for (a = 0; a < length; a++){
-if (b[a] == '0'){
-result = (result << 1);
-}
-else if (b[a] == '1'){
-result = (result << 1) | 1;
-}
-else{
-printf("Invalid string!\n");
-return 0;
-}
-}
+int a;
+unsigned int result = 0;
+int length = strlen("b");
+	if (b == NULL){
+	printf("Empty string\n");
+	return 0;
+	}
+		for (a = 0; a < length; a++){
+		if (b[a] == '0'){
+		result = (result << 1);
+		}
+	else if (b[a] == '1'){
+	result = (result << 1) | 1;
+	}
+	else{
+	printf("Invalid string!\n");
+	return 0;
+	}
+		}
 return result;
 }
 
-int main (){
-const char *b = "110101";
-unsigned int result;
-
-printf("Binary: %s\n", chat *b);
-printf("Deciamal: %u\n", result);
-return 0;
-}
